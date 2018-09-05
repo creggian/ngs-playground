1be293709348ac230f693b532606d6f12bf58073
creggian <claudio.reggiani@ulb.ac.be>
2018-09-05 13:21:38 +0200

    Playground: gatk split mixed multiallelic variant

---
+++ playground/gatk/split_ma_mixed.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+cd "${0%/*}"
+WDIR=`pwd`
+
+source "${WDIR}/../../config/global.bash"
+
+INPUT_VCF="${WDIR}/../../data/variant_ma_mixed.vcf"
+
+${GATK_BIN} \
+    -T LeftAlignAndTrimVariants \
+    -R ${REF_FASTA} \
+    --variant ${INPUT_VCF} \
+    -o output.vcf \
+    --splitMultiallelics
