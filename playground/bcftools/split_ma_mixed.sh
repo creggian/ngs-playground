12ae1e37f156cc9b4b326c825fcb232935c78284
creggian <claudio.reggiani@ulb.ac.be>
2018-09-06 14:20:45 +0200

    Fixed GATK AD header in bcftools split multiallelic

--- playground/bcftools/split_ma_mixed.sh
+++ playground/bcftools/split_ma_mixed.sh
@@ -7,5 +7,7 @@ source "${WDIR}/../../config/global.bash"
 
 INPUT_VCF="${WDIR}/../../data/variant_ma_mixed.vcf"
 
-${BCFTOOLS_BIN} norm -m - ${INPUT_VCF} > temp.vcf
+# the sed is required to fix GATK header of AD
+sed 's/ID=AD,Number=./ID=AD,Number=R/' ${INPUT_VCF} > temp_fix.vcf
+${BCFTOOLS_BIN} norm -m - temp_fix.vcf > temp.vcf
 ${BCFTOOLS_BIN} norm -f ${REF_FASTA} temp.vcf > output.vcf
