10f5fd7abc99a43f08f33bcb6863c52bdd8c8fc9
creggian <claudio.reggiani@ulb.ac.be>
2018-09-10 12:35:35 +0200

    Completed bcftools isec_multiallelic module

--- playground/bcftools/isec_multiallelic/run.sh
+++ playground/bcftools/isec_multiallelic/run.sh
@@ -16,62 +16,43 @@ ${BGZIP_BIN} -c -f ${INPUT_VCF_2} > ${INPUT_VCF_2}.gz
 ${TABIX_BIN} -f -p vcf ${INPUT_VCF_1}.gz
 ${TABIX_BIN} -f -p vcf ${INPUT_VCF_2}.gz
 
-${BCFTOOLS_BIN} \
-        isec \
-	-c both \
-        -O v \
-        -n=2 \
-        -w 1 \
-	${INPUT_VCF_1}.gz \
-	${INPUT_VCF_2}.gz \
-        -o both_common.vcf
+function run_bcftools {
+	local mode="$1"
+	${BCFTOOLS_BIN} \
+		isec \
+		-c ${mode} \
+		-O v \
+		-n=2 \
+		-w 1 \
+		${INPUT_VCF_1}.gz \
+		${INPUT_VCF_2}.gz \
+		-o ${mode}_common.vcf
 
-${BCFTOOLS_BIN} \
-        isec \
-	-c both \
-        -O v \
-        -C \
-        -w 1 \
-        ${INPUT_VCF_1}.gz \
-        ${INPUT_VCF_2}.gz \
-        -o both_first_only.vcf
+	${BCFTOOLS_BIN} \
+		isec \
+		-c ${mode} \
+		-O v \
+		-C \
+		-w 1 \
+		${INPUT_VCF_1}.gz \
+		${INPUT_VCF_2}.gz \
+		-o ${mode}_first_only.vcf
 
-${BCFTOOLS_BIN} \
-        isec \
-        -c both \
-        -O v \
-        -C \
-        -w 1 \
-        ${INPUT_VCF_2}.gz \
-        ${INPUT_VCF_1}.gz \
-        -o both_second_only.vcf
+	${BCFTOOLS_BIN} \
+		isec \
+		-c ${mode} \
+		-O v \
+		-C \
+		-w 1 \
+		${INPUT_VCF_2}.gz \
+		${INPUT_VCF_1}.gz \
+		-o ${mode}_second_only.vcf
+}
 
-${BCFTOOLS_BIN} \
-        isec \
-        -c all \
-        -O v \
-        -n=2 \
-        -w 1 \
-        ${INPUT_VCF_1}.gz \
-        ${INPUT_VCF_2}.gz \
-        -o all_common.vcf
+run_bcftools "none"
+run_bcftools "some"
+run_bcftools "all"
+run_bcftools "snps"
+run_bcftools "indels"
 
-${BCFTOOLS_BIN} \
-        isec \
-        -c all \
-        -O v \
-        -C \
-        -w 1 \
-        ${INPUT_VCF_1}.gz \
-        ${INPUT_VCF_2}.gz \
-        -o all_first_only.vcf
 
-${BCFTOOLS_BIN} \
-        isec \
-        -c all \
-        -O v \
-        -C \
-        -w 1 \
-        ${INPUT_VCF_2}.gz \
-        ${INPUT_VCF_1}.gz \
-        -o all_second_only.vcf
